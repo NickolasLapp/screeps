23d69816a1f14e409414268618f972a3d2574683
Nick <nlapp@argo.ai>
2020-06-03 23:19:29 -0400

    add spawner

---
+++ src/spawner.js
@@ -0,0 +1,13 @@
+var spawner = {
+    /** @param {spawn} spawn**/
+    run: function(spawn) {
+      var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+      console.log('Harvesters: ' + harvesters.length);
+
+      if(harvesters.length < 2) {
+          var new_name = 'Harvester' + Game.time;
+          console.log('Spawning new harvester: ' + new_name);
+          Game.spawns['Spawn1'].spawnCreep([WORK,CARRY,MOVE], new_name,
+              {memory: {role: 'harvester'}});
+      }
+};
